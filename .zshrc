be596d38e1858bc9de4d7648028f35ca89a2462d
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-12-03 13:04:51 +0200

    Add nvim, .aliases, .zshrc

--- .zshrc
+++ .zshrc
@@ -53,3 +53,9 @@ export PATH="$HOME/.tmuxifier/bin:$PATH"
 eval "$(tmuxifier init -)"
 
 # source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/mykyta/Apps/google-cloud-sdk/path.zsh.inc' ]; then . '/home/mykyta/Apps/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/mykyta/Apps/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/mykyta/Apps/google-cloud-sdk/completion.zsh.inc'; fi
--- nvim/.config/nvim/lua/plugins/typescript-tools.lua
+++ nvim/.config/nvim/lua/plugins/typescript-tools.lua
@@ -3,15 +3,15 @@ return {
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   opts = {},
   config = function()
-    require("typescript-tools").setup({
-      handers = {
-        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-          underline = true,
-          virtual_text = true,
-          signs = true,
-          update_in_insert = true,
-        }),
-      }
-    })
+  require('typescript-tools').setup {
+    handers = {
+      ['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline = true,
+        virtual_text = true,
+        signs = true,
+        update_in_insert = true,
+      }),
+    },
+  }
   end,
 }
