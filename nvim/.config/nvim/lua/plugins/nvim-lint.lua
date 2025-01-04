57dc110bc04ebea2965077b6e539a9d1fc8712ab
siriknikita <mykyta.siryk@lnu.edu.ua>
2025-01-04 16:48:22 +0200

    Add nvim

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -5,7 +5,7 @@ return {
     'BufNewFile',
   },
   config = function()
-    local lint = require('lint')
+    local lint = require 'lint'
 
     lint.linters_by_ft = {
       javascript = { 'eslint_d' },
@@ -15,5 +15,13 @@ return {
       svelte = { 'eslint_d' },
       python = { 'pylint' },
     }
+
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
   end,
 }
